ec65742732f2c3b44af86d08eb8cfab797810238
Thomas Altstidl <10142068+taltstidl@users.noreply.github.com>
2025-07-16 15:46:00 +0200

    Fix batching issue in C index

--- src/tabicl/train/metrics.py
+++ src/tabicl/train/metrics.py
@@ -5,4 +5,4 @@ def concordance_index(risk: torch.Tensor, event: torch.Tensor, time: torch.Tenso
     risk_comp = risk.unsqueeze(-1) < risk.unsqueeze(-2)
     time_comp = time.unsqueeze(-1) > time.unsqueeze(-2)
     event = event.unsqueeze(-2)
-    return torch.sum(risk_comp & time_comp & event) / torch.sum(time_comp & event)
+    return torch.sum(risk_comp & time_comp & event, (-2, -1)) / torch.sum(time_comp & event, (-2, -1))
